6377ccf9361b8da9d9115ef4c815e38c7db030a4
Larmen <anders.larm@gmail.com>
2014-04-12 15:26:32 +0200

    Testing first collab commit från eGit (Linux Mint 16, Eclipse ADT).

--- WhosHome/src/com/larmen/whoshome/MainActivity.java
+++ WhosHome/src/com/larmen/whoshome/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.Menu;
  * @author Petter
  */
 public class MainActivity extends Activity {
+	
+	//Testing first commit
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
